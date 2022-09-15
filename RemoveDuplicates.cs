fa5dbcc240e400910d696b0332f558fbec1f1e18
Igor Osipov <62898482+garinich777@users.noreply.github.com>
2022-09-15 17:47:35 +0300

    Add files via upload

---
+++ RemoveDuplicates.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+void RemoveDupls(StringBuilder str)
+{
+    for (int i = 1; i < str.Length; i++)
+        if (str[i] == str[i - 1])
+            str.Remove(i--, 1);
+}
+
+var str = new StringBuilder("AAA BBB CCCC DSASDASDASDD//zz``");
+RemoveDupls(str);
+
+Console.WriteLine(str);
+
+//Игорь Осипов
+//15.09.2022
+//15 минут
